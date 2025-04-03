dbb5ed39552e41da6132b8068491ca7214869018
Lucas Simon <lucasimonmendes@hotmail.com>
2025-04-03 12:56:32 -0300

    :sparkles: refact dashboard, payment-success pages

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,16 +5,29 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 import { auth } from "@/auth";
 
 export default async function Dashboard() {
   const session = await auth();
 
+  if (!session)
+    return (
+      <div className="w-96 mx-auto bg-red-500 m-6 p-6 text-white border border-red-500 rounded-2xl ">
+        <h1 className="text-4xl font-bold m-2">
+          Apenas usuários autenticados!!!
+        </h1>{" "}
+        Por favor, faça login clicando
+        <Link className="font-bold text-green-500 m-2" href="/login">
+          aqui.
+        </Link>
+      </div>
+    );
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-4xl font-bold mb-8">
-        Dashboard de Conteúdo sobre Patos {session?.user?.name}
+        Dashboard de Conteúdo sobre Patos | {session?.user?.name}
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <Card>
--- app/payment-success/page.tsx
+++ app/payment-success/page.tsx
@@ -3,11 +3,19 @@ export default function PaymentSuccess({
 }: {
   searchParams: { amount: string };
 }) {
+  let product;
+  if (amount == "29.99") {
+    product = "Guia completo sobre Patos";
+  } else if (amount == "39.99") {
+    product = "Fotografia de Patos";
+  } else {
+    product = "Cuidados  com Patos de Estimação";
+  }
   return (
     <main className="max-w-6xl mx-auto p-10 text-white text-center border m-10 rounded-md bg-gradient-to-tr from-green-500 to-yellow-500">
       <div className="mb-10">
-        <h1 className="text-4xl font-extrabold mb-2">Thank you!</h1>
-        <h2 className="text-2xl">You successfully sent</h2>
+        <h1 className="text-4xl font-extrabold mb-2">Muito obrigado!</h1>
+        <h2 className="text-2xl">Você acaba de adquirir {product}</h2>
 
         <div className="bg-black p-2 rounded-md text-green-500 mt-5 text-4xl font-bold">
           R${amount}
